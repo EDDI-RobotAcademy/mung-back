b1c072fca566b76c09cf579d40e102da3615fb6b
HEEJI YUN <yhj8147e@gmail.com>
2023-02-28 23:01:56 +0900

    feat: [윤희지] member entity 컬럼 수정, 일반 회원가입용 생성자 추가 [mung-back2]

--- src/main/kotlin/com/example/mungmung/member/entity/Member.kt
+++ src/main/kotlin/com/example/mungmung/member/entity/Member.kt
@@ -1,31 +1,42 @@
 package com.example.mungmung.member.entity
 
+import com.example.mungmung.security.entity.Authentication
 import jakarta.persistence.*
 import lombok.Getter
-import lombok.NoArgsConstructor
 import lombok.Setter
 
 @Entity
-@NoArgsConstructor
 @Setter
 @Getter
-class Member {
+class Member() {
 
     @Id
     @Column(name = "member_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private val id: Long? = null
+    private var id: Long? = null
 
     @Column(nullable = false)
-    private val email: String? = null
+    private var email: String? = null
 
     @Column(nullable = false)
-    private val username: String? = null
+    private var nickname: String? = null
 
     @Column(nullable = false)
-    private val birthdate = 0
+    private var socialType:SocialType? = null
 
     @Column(nullable = false)
-    private val socialType:SocialType? = null
+    private var memberType: MemberType? = null
 
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY, cascade = [CascadeType.REMOVE])
+    private val authentications: Set<Authentication> = HashSet<Authentication>()
+
+    constructor(email: String, nickname: String, memberType: String) : this() {
+        this.email = email
+        this.nickname = nickname
+        when(memberType) {
+            MemberType.NORMAL.type -> this.memberType = MemberType.NORMAL
+            MemberType.ADMIN.type -> this.memberType = MemberType.ADMIN
+        }
+        this.socialType = SocialType.LOCAL
+    }
 }
