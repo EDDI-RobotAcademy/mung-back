ca093c70f81c2be625d1c2da65e0517b399c5d2d
choi jaemin <foertonw5@naver.com>
2023-02-28 18:21:08 +0900

    feat :[최재민] mung Wiki 에 초기 데이터를 등록하기 위한 register 서비스[mung-back8]

--- src/main/kotlin/com/example/mungmung/mungWiki/service/MungWikiServiceImpl.kt
+++ src/main/kotlin/com/example/mungmung/mungWiki/service/MungWikiServiceImpl.kt
@@ -1,11 +1,37 @@
 package com.example.mungmung.mungWiki.service
 
-
+import com.example.mungmung.mungWiki.entity.DogStatus
+import com.example.mungmung.mungWiki.entity.MungWiki
+import com.example.mungmung.mungWiki.repository.DogStatusRepository
+import com.example.mungmung.mungWiki.repository.MungWikiRepository
+import com.example.mungmung.mungWiki.repository.WikiDocumentRepository
+import com.example.mungmung.mungWiki.request.RegisterRequest
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class MungWikiServiceImpl {
 
+    @Autowired
+    private lateinit var mungWikiRepository: MungWikiRepository
 
+    @Autowired
+    private lateinit var dogStatusRepository: DogStatusRepository
+
+    @Autowired
+    private lateinit var wikiDocumentRepository: WikiDocumentRepository
+
+    fun registerWiki(request: RegisterRequest) {
+
+        val dogStatus = request.toDogStatue()
+        val wikiDocument = request.toWikiDocument()
+
+        dogStatusRepository.save(dogStatus)
+        wikiDocumentRepository.save(wikiDocument)
+
+
+        val mungWiki = MungWiki(request.dogType,request.dogImgName,wikiDocument ,dogStatus)
+        mungWikiRepository.save(mungWiki)
+    }
 
 }
